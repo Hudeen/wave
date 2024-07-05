22279b299a18e30c07785f42bc8f8e5446c4056a
Huden <klaud.i@mail.ru>
2024-07-05 19:11:35 +0300

    fix: mobile menu

--- src/components/ui/burgermenu/Burgermenu.tsx
+++ src/components/ui/burgermenu/Burgermenu.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Burgermenu.scss';
-import logo from '../../../assets/icon/logo.svg'
+import logo from '../../../assets/icon/logo.svg';
 
 const BurgerMenu: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -8,6 +8,30 @@ const BurgerMenu: React.FC = () => {
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
+
+  const handleNavClick = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>, targetId: string) => {
+    event.preventDefault();
+    const targetElement = document.getElementById(targetId);
+    if (targetElement) {
+        window.scrollTo({
+            top: targetElement.offsetTop,
+            behavior: 'smooth'
+        });
+    }
+    toggleMenu(); // Закрыть меню после клика
+  };
+
+  const handleExternalLinkClick = () => {
+    toggleMenu(); // Закрыть меню после клика на внешнюю ссылку
+  };
+
+  useEffect(() => {
+    if (isOpen) {
+      document.body.classList.add('no-scroll');
+    } else {
+      document.body.classList.remove('no-scroll');
+    }
+  }, [isOpen]);
 
   return (
     <>
@@ -23,11 +47,11 @@ const BurgerMenu: React.FC = () => {
           </div>
           <nav>
             <ul>
-              <li><a href="#tickets">БИЛЕТЫ</a></li>
-              <li><a href="#locations">ЛОКАЦИИ</a></li>
-              <li><a href="#headliners">ХЕДЛАЙНЕРЫ</a></li>
-              <li><a href="#photos">ФОТО</a></li>
-              <li><a href="#market">МАРКЕТ</a></li>
+              <li><a href='https://geopro-event.timepad.ru/event/2842320/' onClick={handleExternalLinkClick}>БИЛЕТЫ</a></li>
+              <li><a onClick={(e) => handleNavClick(e, 'locations')}>ЛОКАЦИИ</a></li>
+              <li><a onClick={(e) => handleNavClick(e, 'headliners')}>ХЕДЛАЙНЕРЫ</a></li>
+              {/* <li><a onClick={(e) => handleNavClick(e, 'photos')}>ФОТО</a></li> */}
+              <li><a href="https://forms.gle/iwzT351qpKfNFA15A">МАРКЕТ</a></li>
             </ul>
           </nav>
         </div>
